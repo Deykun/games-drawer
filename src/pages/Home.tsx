e2260f2ac4700d6c0a8c08ef1095f064519e32df
Szymon Tondowski <stdeykun+git@gmail.com>
2024-07-30 23:31:37 +0200

    Social data added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,9 +6,9 @@ const Home = () => {
           <h1 className="mt-10 text-lg">My random attempts to write some Canvas logic.</h1>
           <br />
           <ol>
-            <li><Link to="/games-drawer/rover" className="text-lg text-blue-700">01. Rover</Link></li>
-            <li><Link to="/games-drawer/blocks" className="text-lg text-blue-700">02. Blocks</Link></li>
-            <li><Link to="/games-drawer/gecko" className="text-lg text-blue-700">03. Gecko</Link></li>
+            <li><Link to="/games-drawer/rover" className="text-lg text-blue-700">01. 🌕 Rover</Link></li>
+            <li><Link to="/games-drawer/blocks" className="text-lg text-blue-700">02. 🟩 Blocks</Link></li>
+            <li><Link to="/games-drawer/gecko" className="text-lg text-blue-700">03. 🦎 Gecko</Link></li>
           </ol>
         </>
     )
