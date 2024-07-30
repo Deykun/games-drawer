2150d6d4cd5b1f7b132f25d2bff9532afd9d6c71
Szymon Tondowski <stdeykun+git@gmail.com>
2024-07-30 04:21:42 +0200

    Path fied for ssr

--- scripts/post-build.ts
+++ scripts/post-build.ts
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-const PATHS_TO_ADD_TO_SSR = ['cover'];
+const PATHS_TO_ADD_TO_SSR = ['rover'];
 
 PATHS_TO_ADD_TO_SSR.forEach((path) => {
   const html = fs.readFileSync('./dist/index.html', 'utf-8');
