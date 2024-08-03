37751ae3b752dd0a1f3c05db6468b52878b710ed
Szymon Tondowski <stdeykun+git@gmail.com>
2024-08-03 19:50:26 +0200

    Better matcher of clicked block - Wip

--- src/pages/02-blocks/utils/isometric.ts
+++ src/pages/02-blocks/utils/isometric.ts
@@ -7,17 +7,34 @@ export const getPositionFromXY = ({
   screenOffsetY,
   zoomLevel,
 }: { x: number, y: number, screenOffsetX: number, screenOffsetY: number, zoomLevel: number }) => {
+  
   const canvasTileWidth = zoomLevel * tileWidth;
   // const canvasTileWidth = tileWidth;
 
   const canvasX = x - screenOffsetX;
   const canvasY = y - screenOffsetY;
 
+
+   // Calculate isoX and isoY
+   const isoX = canvasY - (canvasX / 2);
+   const isoY = canvasY + (canvasX / 2);
+   
+   // Calculate x and y
+   const positionXraw = (2 * isoX) / canvasTileWidth;
+   const positionYraw = (2 * isoY) / canvasTileWidth;
+
+    // Calculate x and y
+    const positionX = positionXraw + 1;
+    const positionY = positionYraw + 4;
+   
+  //  return { x, y };
+
+
   // const positionX = (canvasY - canvasX) / canvasTileWidth;
   // const positionY = (canvasX + canvasY) / canvasTileWidth;
 
-  const positionX = Math.floor((canvasY - canvasX) / canvasTileWidth) + 1;
-  const positionY = Math.floor((canvasX + canvasY) / canvasTileWidth) + 4;
+  // const positionX = Math.floor((canvasY - canvasX) / canvasTileWidth) + 1;
+  // const positionY = Math.floor((canvasX + canvasY) / canvasTileWidth) + 4;
 
   const location = `${Math.floor(positionX)}x${Math.floor(positionY)}`;
 
