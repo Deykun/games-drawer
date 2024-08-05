a83d60da25e94505c8f0fc345ae98e8f95449422
Szymon Tondowski <stdeykun+git@gmail.com>
2024-08-05 23:51:04 +0200

    className fixed

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
         <div>
           <ul className="font-medium flex flex-wrap gap-5">
             <li>
-              <Link to="/games-drawer/" className="block py-2 px-3rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Home</Link>
+              <Link to="/games-drawer/" className="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Home</Link>
             </li>
             <li>
               <a href="https://github.com/Deykun/games-drawer" target="_blank" className="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" rel="noreferrer">
